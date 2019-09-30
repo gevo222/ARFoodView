5b2f6407dfb0d04144d9d6c8b23b5b5de69398d0
gevo222 <51228298+gevo222@users.noreply.github.com>
2019-09-30 13:20:27 -0700

    requests permissions

--- app/src/main/java/com/example/arfoodview/MainActivity.java
+++ app/src/main/java/com/example/arfoodview/MainActivity.java
@@ -1,16 +1,59 @@
 package com.example.arfoodview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final int REQUEST_CODE_ASK_PERMISSIONS = 1;
+
+    //This is for requesting permissions
+    private static final String[] RUNTIME_PERMISSIONS = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.INTERNET,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.CAMERA
+    };
+
+    int runtime_request = 1; //used in .requestPermissions()   for error message
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (hasPermissions(this, RUNTIME_PERMISSIONS)) {
+            Log.d("food", "Runtime permissions already granted");
 
+        } else {
+            Log.d("food", "Runtime permissions not granted");
+            ActivityCompat.requestPermissions(this, RUNTIME_PERMISSIONS, runtime_request);
+        }
+
+    }
+
+    private static boolean hasPermissions(Context context, String... permissions) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        } else {
+            Log.d("food", "OS Build Version Not Met");
+        }
+        return true;
     }
 }
