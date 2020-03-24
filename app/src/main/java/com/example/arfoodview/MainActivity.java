c016f02a068607d09aa3c5f44a20415b2918fcbf
MoRaza <mohammedraza24@gmail.com>
2020-03-24 14:14:43 -0700

    Added Map Fragment

--- app/src/main/java/com/example/arfoodview/MainActivity.java
+++ app/src/main/java/com/example/arfoodview/MainActivity.java
@@ -12,6 +12,17 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import android.os.Bundle;
+
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -29,10 +40,14 @@ import javax.annotation.Nullable;
 import in.galaxyofandroid.spinerdialog.OnSpinerItemClick;
 import in.galaxyofandroid.spinerdialog.SpinnerDialog;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
 
     private static final int REQUEST_CODE_ASK_PERMISSIONS = 1;
+
+    private GoogleMap mMap;
 
     //public static String sel_rest = "";
 
@@ -165,7 +180,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync((OnMapReadyCallback) this);
     }//end of onCreate
+
+
+    //@Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
 
     private void initRestaurant() {
 
---
+++ app/src/main/java/com/example/arfoodview/MapsActivity.java
@@ -0,0 +1,36 @@
+package com.example.arfoodview;
+
+import android.os.Bundle;
+// import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+    public class MapsActivity extends MainActivity implements OnMapReadyCallback {
+
+        private GoogleMap mMap;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_maps);
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
+
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            //map is ready to be used
+            mMap = googleMap;
+
+            // Add a marker in Sydney, Australia, and move the camera.
+            LatLng sydney = new LatLng(-34, 151);
+            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        }
+    }
+
