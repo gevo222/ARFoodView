bf81f6c4ecdccd74472cc979b89c0051255474b8
MoRaza <mohammedraza24@gmail.com>
2020-04-22 11:24:12 -0700

    Added Map Fragment and code

--- app/src/main/java/com/example/arfoodview/MapsActivity4.java
+++ app/src/main/java/com/example/arfoodview/MapsActivity4.java
@@ -33,7 +33,7 @@ public class MapsActivity4 extends FragmentActivity implements OnMapReadyCallbac
      * we just add a marker near Sydney, Australia.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     * installed Google Play services andreturned to the app.
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
