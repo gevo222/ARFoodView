cb9ef539e56801283b60ddd053396a717eeb9244
gevo222 <gevo223@gmail.com>
2021-06-14 00:03:50 -0700

    Cleaned up SeeIngredients

--- app/src/main/java/com/example/arfoodview/SeeIngredients.java
+++ app/src/main/java/com/example/arfoodview/SeeIngredients.java
@@ -5,13 +5,9 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,8 +15,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
@@ -34,16 +28,15 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class SeeIngredients extends AppCompatActivity{
-    private  FirebaseFirestore db;
+public class SeeIngredients extends AppCompatActivity {
     final String TAG = "firebaselog";
     TextView fName;
     TextView allDisplay;
     TextView ings;
-    ListView ingredients;
     List<String> userAllergies; // user given by users
-    List<String> allergentCross; // user given by users
+    ArrayList<String> allergentCross; // user given by users
     FirebaseAuth fAuth;
 
     @Override
@@ -54,42 +47,19 @@ public class SeeIngredients extends AppCompatActivity{
         String itemChosen = Menu_Window.chosenItem; //getIntent().getStringExtra("itemChosen");
         userAllergies = new ArrayList<>();
         // populating the textViews
-        db = FirebaseFirestore.getInstance();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         setContentView(R.layout.ingredients_list_item);
-        fName = (TextView)findViewById(R.id.displayFoodName);
-        ings = (TextView)findViewById(R.id.textArea);
-        allDisplay = (TextView)findViewById( R.id.allergendisplay );
+        fName = (TextView) findViewById(R.id.displayFoodName);
+        ings = (TextView) findViewById(R.id.textArea);
+        allDisplay = (TextView) findViewById(R.id.allergendisplay);
         // end of populating the textViews
 
         String restPath = "restaurants/" + restChosen + "/food";
 
         // new DB
         fAuth = FirebaseAuth.getInstance();
-        String user_id = fAuth.getCurrentUser().getUid();
+        String user_id = Objects.requireNonNull(fAuth.getCurrentUser()).getUid();
         DatabaseReference mRef = FirebaseDatabase.getInstance().getReference().child("Userss").child(user_id).child("allergies");
-
-        // this is done to get the allergens
-        /*DocumentReference docAllergyref = db.collection("users").document("temporary");
-        docAllergyref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    userAllergies = (List<String>) document.get( "allergies" );
-                    if (document.exists()) {
-                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-                        userAllergies = (ArrayList<String>) document.get("allergies");
-                        Log.d(TAG, "DocumentSnapshot data: " + document.get("allergies"));
-                        Log.d(TAG, ""+userAllergies.contains("Peanuts"));
-                    } else {
-                        Log.d(TAG, "No such document");
-                    }
-                } else {
-                    Log.d(TAG, "get failed with ", task.getException());
-                }
-            }
-        });*/
-
 
         mRef.addChildEventListener(new ChildEventListener() {
             @Override
@@ -124,125 +94,114 @@ public class SeeIngredients extends AppCompatActivity{
 
         DocumentReference docRef = db.collection(restPath).document(itemChosen);
 
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    List<String> ing = (List<String>) document.get("ingredients");
-                    //for allergens
-                    List<String> allergenData = (List<String>) document.get( "allergens" );
-                    allergentCross = new ArrayList(  );
+        docRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                assert document != null;
+                List<String> ing = (List<String>) document.get("ingredients");
+                //for allergens
+                List<String> allergenData = (List<String>) document.get("allergens");
+                allergentCross = new ArrayList<>();
 
-                    for(int i = 0; i < userAllergies.size(); i++) {
-                        for (int j = 0; j < allergenData.size(); j++) {
-                            if (userAllergies.get(i).equals(allergenData.get(j))) {
-                                allergentCross.add(userAllergies.get(i));
-                            }
+                for (int i = 0; i < userAllergies.size(); i++) {
+                    for (int j = 0; j < Objects.requireNonNull(allergenData).size(); j++) {
+                        if (userAllergies.get(i).equals(allergenData.get(j))) {
+                            allergentCross.add(userAllergies.get(i));
                         }
                     }
+                }
 
-                    Log.d( TAG,"Cross data is: "+ allergentCross );
-                    if(!allergentCross.isEmpty()){
-                        allDisplay.setTextColor(Color.parseColor("#FF6400"));
-                        //allDisplay.setTextColor(color.RED);
-                        allDisplay.append( "Allergens:" );
-                        allDisplay.setTypeface( Typeface.DEFAULT_BOLD);
-                        for(String str : allergentCross){
-                            allDisplay.append( "\n"+str );
-                        }
-                    }else{
-                        allDisplay.setText("No Allergens.");
-                    }
-
-
-                    if (document.exists()) {
-                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-                        Log.d(TAG, "DocumentSnapshot data: " + document.get("calories"));
-                        Log.d(TAG, "DocumentSnapshot dataAllergens: " + document.get("allergens"));
-                        fName.setText(itemChosen);
-
-                        // choosing the right 2D images
-                        ImageView img = (ImageView)findViewById( R.id.imageview_2 );
-                        switch (itemChosen) {
-                            case "Ramen":
-                                img.setImageResource(R.drawable.ramens);
-                                break;
-                            case "salmon":
-                                img.setImageResource(R.drawable.salmon);
-                                break;
-                            case "applePie":
-                                img.setImageResource(R.drawable.applepie);
-                                break;
-                            case "pancakes":
-                                img.setImageResource(R.drawable.pancakes);
-                                break;
-                            case "Candy Bowl":
-                                img.setImageResource(R.drawable.candybowl);
-                                break;
-                            case "Pizza":
-                                img.setImageResource(R.drawable.pizza);
-                                break;
-                            case "Apple Strudel":
-                                img.setImageResource(R.drawable.applestrudel);
-                                break;
-                            default:
-                                System.out.println("image not found!");
-                                break;
-                        }
-                        // end of choosing 2D images
-
-                        for(String str : ing){
-                            if(true) { // need to work on this
-                                ings.append("\n" + str);
-                            }else{
-                                ings.append("\n" + str);
-                            }
-                        }
-                    } else {
-                        Log.d(TAG, "No such document");
+                Log.d(TAG, "Cross data is: " + allergentCross);
+                if (!allergentCross.isEmpty()) {
+                    allDisplay.setTextColor(Color.parseColor("#FF6400"));
+                    //allDisplay.setTextColor(color.RED);
+                    allDisplay.append("Allergens:");
+                    allDisplay.setTypeface(Typeface.DEFAULT_BOLD);
+                    for (String str : allergentCross) {
+                        allDisplay.append("\n" + str);
                     }
                 } else {
-                    Log.d(TAG, "get failed with ", task.getException());
+                    allDisplay.setText("No Allergens.");
                 }
+
+
+                if (document.exists()) {
+                    Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                    Log.d(TAG, "DocumentSnapshot data: " + document.get("calories"));
+                    Log.d(TAG, "DocumentSnapshot dataAllergens: " + document.get("allergens"));
+                    fName.setText(itemChosen);
+
+                    // choosing the right 2D images
+                    ImageView img = (ImageView) findViewById(R.id.imageview_2);
+                    switch (itemChosen) {
+                        case "Ramen":
+                            img.setImageResource(R.drawable.ramens);
+                            break;
+                        case "salmon":
+                            img.setImageResource(R.drawable.salmon);
+                            break;
+                        case "applePie":
+                            img.setImageResource(R.drawable.applepie);
+                            break;
+                        case "pancakes":
+                            img.setImageResource(R.drawable.pancakes);
+                            break;
+                        case "Candy Bowl":
+                            img.setImageResource(R.drawable.candybowl);
+                            break;
+                        case "Pizza":
+                            img.setImageResource(R.drawable.pizza);
+                            break;
+                        case "Apple Strudel":
+                            img.setImageResource(R.drawable.applestrudel);
+                            break;
+                        default:
+                            System.out.println("image not found!");
+                            break;
+                    }
+                    // end of choosing 2D images
+
+                    assert ing != null;
+                    for (String str : ing) {
+                        ings.append("\n" + str);
+                    }
+                } else {
+                    Log.d(TAG, "No such document");
+                }
+            } else {
+                Log.d(TAG, "get failed with ", task.getException());
             }
         });
 
         // help button
         final ImageButton helpButton = findViewById(R.id.HelpButton);
-        helpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            // When user settings start button do this
-            public void onClick(View view) {
-                Log.d("VisiFood", "Clicked Settings Button");
-                Intent intent = new Intent(SeeIngredients.this, helpActivity.class);
-                startActivity(intent);
-                //Intent intent2 = new Intent(MainActivity.this, AllergyActivity.class);
+        // When user settings start button do this
+        helpButton.setOnClickListener(view -> {
+            Log.d("VisiFood", "Clicked Settings Button");
+            Intent intent = new Intent(SeeIngredients.this, helpActivity.class);
+            startActivity(intent);
+            //Intent intent2 = new Intent(MainActivity.this, AllergyActivity.class);
 
-            }
         });
 
         // Navigation Bar
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Intent intent;
-                switch (item.getItemId()) {
-                    case R.id.AR_item:
-                        Toast.makeText(SeeIngredients.this, "View AR", Toast.LENGTH_SHORT).show();
-                        intent = new Intent(SeeIngredients.this, Ar.class);
-                        startActivity(intent);
-                        break;
-                    case R.id.nutrition_item:
-                        Toast.makeText(SeeIngredients.this, "Nutrition Info", Toast.LENGTH_SHORT).show();
-                        intent = new Intent(SeeIngredients.this, Infor_window.class);
-                        startActivity(intent);
-                        finish();
-                        break;
-                }
-                return true;
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Intent intent;
+            switch (item.getItemId()) {
+                case R.id.AR_item:
+                    Toast.makeText(SeeIngredients.this, "View AR", Toast.LENGTH_SHORT).show();
+                    intent = new Intent(SeeIngredients.this, Ar.class);
+                    startActivity(intent);
+                    break;
+                case R.id.nutrition_item:
+                    Toast.makeText(SeeIngredients.this, "Nutrition Info", Toast.LENGTH_SHORT).show();
+                    intent = new Intent(SeeIngredients.this, Infor_window.class);
+                    startActivity(intent);
+                    finish();
+                    break;
             }
+            return true;
         });
 
         // to hide android's nav bar
