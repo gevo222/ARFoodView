1c2d38282fc5fa185236c4ff61144d82b762cc91
gevo222 <gevo223@gmail.com>
2021-06-13 21:58:03 -0700

    App starts on Login Page

--- app/src/main/java/com/example/arfoodview/SplashScreen.java
+++ app/src/main/java/com/example/arfoodview/SplashScreen.java
@@ -31,7 +31,7 @@ public class SplashScreen extends Activity {
             public void run() {
                 // This method will be executed once the timer is over
                 // Start your app main activity
-                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                Intent i = new Intent(SplashScreen.this, LoginActivity.class);
                 startActivity(i);
 
                 // close this activity
