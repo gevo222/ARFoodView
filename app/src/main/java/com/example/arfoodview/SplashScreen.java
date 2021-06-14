6c011f0c1c640353f631be0a6db0e2ac89413e69
gevo222 <gevo223@gmail.com>
2021-06-14 00:04:13 -0700

    Cleaned up SplashScreen

--- app/src/main/java/com/example/arfoodview/SplashScreen.java
+++ app/src/main/java/com/example/arfoodview/SplashScreen.java
@@ -9,8 +9,9 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashScreen extends Activity {
     // Splash screen timer
-    private static int SPLASH_TIME_OUT = 1000;
+    private static final int SPLASH_TIME_OUT = 1000;
     FirebaseAuth fAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,23 +21,18 @@ public class SplashScreen extends Activity {
         fAuth = FirebaseAuth.getInstance();
         fAuth.signOut();
 
-        new Handler().postDelayed(new Runnable() {
+        /*
+         * Showing splash screen with a timer. This will be useful when you
+         * want to show case your app logo / company
+         */
+        new Handler().postDelayed(() -> {
+            // This method will be executed once the timer is over
+            // Start your app main activity
+            Intent i = new Intent(SplashScreen.this, LoginActivity.class);
+            startActivity(i);
 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
-
-            @Override
-            public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashScreen.this, LoginActivity.class);
-                startActivity(i);
-
-                // close this activity
-                finish();
-            }
+            // close this activity
+            finish();
         }, SPLASH_TIME_OUT);
     }
 }
