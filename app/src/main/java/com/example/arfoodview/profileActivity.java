0248cdf7a9c1e46783c21ebc1271e5adc888166a
gevo222 <gevo223@gmail.com>
2021-06-14 00:00:32 -0700

    Cleaned up profileActivity

--- app/src/main/java/com/example/arfoodview/profileActivity.java
+++ app/src/main/java/com/example/arfoodview/profileActivity.java
@@ -2,13 +2,10 @@ package com.example.arfoodview;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -30,50 +27,43 @@ public class profileActivity extends AppCompatActivity {
         fAuth = FirebaseAuth.getInstance();
 
         // if user isn't signed in, go to login
-        if(fAuth.getCurrentUser() == null) {
+        if (fAuth.getCurrentUser() == null) {
             startActivity(new Intent(getApplicationContext(), LoginActivity.class));
             finish();
         }
 
 
-        mLogoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
+        mLogoutButton.setOnClickListener(view -> {
+            fAuth.signOut();
+            Intent intent = new Intent(profileActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
 
-            public void onClick(View view) {
-                fAuth.signOut();
-                Intent intent = new Intent(profileActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-
-            }
         });
 
 
         // Navigation Bar
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Intent intent;
-                switch (item.getItemId()) {
-                    case R.id.profile_icon:
-                        Toast.makeText(profileActivity.this, "Profile", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.search_icon:
-                        Toast.makeText(profileActivity.this, "Search", Toast.LENGTH_SHORT).show();
-                        intent = new Intent(profileActivity.this, MainActivity.class);
-                        startActivity(intent);
-                        finish();
-                        break;
-                    case R.id.settings_icon:
-                        Toast.makeText(profileActivity.this, "Settings", Toast.LENGTH_SHORT).show();
-                        intent = new Intent(profileActivity.this, SettingsActivity.class);
-                        startActivity(intent);
-                        finish();
-                        break;
-                }
-                return true;
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Intent intent;
+            switch (item.getItemId()) {
+                case R.id.profile_icon:
+                    Toast.makeText(profileActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.search_icon:
+                    Toast.makeText(profileActivity.this, "Search", Toast.LENGTH_SHORT).show();
+                    intent = new Intent(profileActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    break;
+                case R.id.settings_icon:
+                    Toast.makeText(profileActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                    intent = new Intent(profileActivity.this, SettingsActivity.class);
+                    startActivity(intent);
+                    finish();
+                    break;
             }
+            return true;
         });
         // to hide android's nav bar
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
