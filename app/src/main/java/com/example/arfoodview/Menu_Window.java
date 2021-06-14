e666a21be153a5141257fd590ba574e169f3d62c
gevo222 <gevo223@gmail.com>
2021-06-14 00:00:11 -0700

    Cleaned up Menu_Window

--- app/src/main/java/com/example/arfoodview/Menu_Window.java
+++ app/src/main/java/com/example/arfoodview/Menu_Window.java
@@ -3,50 +3,28 @@ package com.example.arfoodview;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
 public class Menu_Window extends AppCompatActivity {
-   static String model = "";
-   static String chosenRest = "";
-   static String chosenItem = "";
+    static String model = "";
+    static String chosenRest = "";
+    static String chosenItem = "";
     FirebaseFirestore db;
     SearchView searchView;
     ListView listView;
     ArrayList<String> dish;
-    ArrayAdapter<String > adapter;
-    ArrayAdapter<String > arrayAdapter;
-    DatabaseReference databaseReference;
-    FirebaseDatabase firebaseDatabase;
-
-
-//    // public onstructor
-//    public Menu_Window(){
-//
-//        // this will bdo nothing.
-//    }
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,36 +45,35 @@ public class Menu_Window extends AppCompatActivity {
         String TAG = "firebase_menu_window";
 
 
-         db = FirebaseFirestore.getInstance();
+        db = FirebaseFirestore.getInstance();
         String restPath = "restaurants/" + restaurant + "/food";
 
-        db.collection(restPath).addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent( QuerySnapshot documentSnapshots,  FirebaseFirestoreException e) {
+        db.collection(restPath).addSnapshotListener((documentSnapshots, e) -> {
 
-                if(e != null) {
-                    Log.d(TAG, "error: " + e.getMessage());
-                }
+            if (e != null) {
+                Log.d(TAG, "error: " + e.getMessage());
+            }
 
-                for(DocumentSnapshot doc: documentSnapshots) {
+            if (documentSnapshots != null) {
+                for (DocumentSnapshot doc : documentSnapshots) {
                     String itemName = doc.getId();
-                    System.out.println( "itemName: "+itemName );
+                    System.out.println("itemName: " + itemName);
                     dish.add(itemName);
                 }
-                adapter = new ArrayAdapter<String>(Menu_Window.this, android.R.layout.simple_list_item_1,dish);
-                listView.setAdapter(adapter);
-
             }
+            adapter = new ArrayAdapter<>(Menu_Window.this, android.R.layout.simple_list_item_1, dish);
+            listView.setAdapter(adapter);
+
         });
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
 
-                if(dish.contains(query)){
+                if (dish.contains(query)) {
                     adapter.getFilter().filter(query);
-                }else{
-                    Toast.makeText(Menu_Window.this, "No Match found",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(Menu_Window.this, "No Match found", Toast.LENGTH_LONG).show();
                 }
                 return false;
             }
@@ -108,25 +85,22 @@ public class Menu_Window extends AppCompatActivity {
             }
         });
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        listView.setOnItemClickListener((parent, view, position, id) -> {
 
-                String item = ((TextView)view).getText().toString();
-                Intent intent = new Intent(Menu_Window.this, Infor_window.class);
-                chosenRest = restaurant;
-                chosenItem = item;
-                //intent.putExtra("restChosen", restaurant);
-                intent.putExtra("itemChosen", item);
-                startActivity(intent);
-                model = item;
-                System.out.println( "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" );
-                System.out.println( "Model chosen is: "+model );
+            String item = ((TextView) view).getText().toString();
+            Intent intent = new Intent(Menu_Window.this, Infor_window.class);
+            chosenRest = restaurant;
+            chosenItem = item;
+            //intent.putExtra("restChosen", restaurant);
+            intent.putExtra("itemChosen", item);
+            startActivity(intent);
+            model = item;
+            System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            System.out.println("Model chosen is: " + model);
 
-            }
         });
     }
-        //public String getModel(){
-        //return model;
-        //}
+    //public String getModel(){
+    //return model;
+    //}
 }
