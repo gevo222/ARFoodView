d1a26eaa9fffc413c3ca62abbfe63070e3a603ad
gevo222 <gevo223@gmail.com>
2021-06-13 22:21:10 -0700

    Cleaned up LoginActivity

--- app/src/main/java/com/example/arfoodview/LoginActivity.java
+++ app/src/main/java/com/example/arfoodview/LoginActivity.java
@@ -1,34 +1,25 @@
 package com.example.arfoodview;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText mEmailText, mPasswordText;
     Button mRLoginButton, mCreateButton;
     FirebaseAuth fAuth;
-    //Todo: Add progress Bar again
-    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,70 +29,44 @@ public class LoginActivity extends AppCompatActivity {
         Button RegisterBtn = findViewById(R.id.registerBtn);
         Button LoginBtn = findViewById(R.id.loginButton);
 
-        //gevo
         mEmailText = findViewById(R.id.emailText);
         mPasswordText = findViewById(R.id.passwordText);
-        mRLoginButton = findViewById(R.id.loginButton);
-        mCreateButton = findViewById(R.id.registerBtn);
+        mRLoginButton = LoginBtn;
+        mCreateButton = RegisterBtn;
         fAuth = FirebaseAuth.getInstance();
-        //Todo progressBar = something
-
-        //end
 
 
-        LoginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            // When user settings start button do this
-            public void onClick(View view) {
+        // When user settings start button do this
+        LoginBtn.setOnClickListener(view -> {
 
-                String email = mEmailText.getText().toString().trim();
-                String password = mPasswordText.getText().toString().trim();
+            String email = mEmailText.getText().toString().trim();
+            String password = mPasswordText.getText().toString().trim();
 
-                if(TextUtils.isEmpty(email)){
-                    mEmailText.setError("Email is Required");
-                    return;
-                }
-
-                if(TextUtils.isEmpty(password)){
-                    mPasswordText.setError("Password is Required");
-                    return;
-                }
-
-                if(password.length()<6) {
-                    mPasswordText.setError("Password must be 6 or more Characters");
-                    return;
-                }
-                //Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                //startActivity(intent);
-
-                //authenticate the user
-                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(LoginActivity.this, "Logged in Successfully.", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                            startActivity(intent);
-                        }
-                        else {
-                            Toast.makeText(LoginActivity.this,"Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                            // set progress bar to gone 2/4 28:00
-                        }
-
-                    }
-                });
+            if(TextUtils.isEmpty(email)){
+                mEmailText.setError("Email is Required");
+                return;
             }
+
+            if(TextUtils.isEmpty(password)){
+                mPasswordText.setError("Password is Required");
+                return;
+            }
+
+            if(password.length()<6) {
+                mPasswordText.setError("Password must be 6 or more Characters");
+                return;
+            }
+
+            //authenticate the user
+            fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this::onComplete);
         });
 
 
-        RegisterBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            // When user settings start button do this
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
+        // When user settings start button do this
+        RegisterBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
 
-            }
         });
 
         // to hide android's nav bar
@@ -123,4 +88,15 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+    private void onComplete(Task<AuthResult> task) {
+        if (task.isSuccessful()) {
+            Toast.makeText(LoginActivity.this, "Logged in Successfully.", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(LoginActivity.this, "Error!" + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+            // set progress bar to gone 2/4 28:00
+        }
+
+    }
 }
