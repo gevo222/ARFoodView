16f1c6b0134a143a5712b8eacbacbcc0bc47edb5
gevo222 <gevo223@gmail.com>
2021-06-14 00:01:34 -0700

    Cleaned up ReviewActivity

--- app/src/main/java/com/example/arfoodview/ReviewActivity.java
+++ app/src/main/java/com/example/arfoodview/ReviewActivity.java
@@ -3,10 +3,8 @@ package com.example.arfoodview;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -21,31 +19,28 @@ public class ReviewActivity extends AppCompatActivity {
 
         // Navigation Bar
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Intent intent;
-                switch (item.getItemId()) {
-                    case R.id.ingredient_item:
-                        Toast.makeText(ReviewActivity.this, "Reviews", Toast.LENGTH_SHORT).show();
-                        Log.d("NavigationLog", "Clicked Reviews");
-                        intent = new Intent(ReviewActivity.this, SeeIngredients.class);
-                        startActivity(intent);
-                        break;
-                    case R.id.AR_item:
-                        Toast.makeText(ReviewActivity.this, "View AR", Toast.LENGTH_SHORT).show();
-                        intent = new Intent(ReviewActivity.this, Ar.class);
-                        startActivity(intent);
-                        break;
-                    case R.id.nutrition_item:
-                        Toast.makeText(ReviewActivity.this, "Nutrition Info", Toast.LENGTH_SHORT).show();
-                        intent = new Intent(ReviewActivity.this, Infor_window.class);
-                        startActivity(intent);
-                        finish();
-                        break;
-                }
-                return true;
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Intent intent;
+            switch (item.getItemId()) {
+                case R.id.ingredient_item:
+                    Toast.makeText(ReviewActivity.this, "Reviews", Toast.LENGTH_SHORT).show();
+                    Log.d("NavigationLog", "Clicked Reviews");
+                    intent = new Intent(ReviewActivity.this, SeeIngredients.class);
+                    startActivity(intent);
+                    break;
+                case R.id.AR_item:
+                    Toast.makeText(ReviewActivity.this, "View AR", Toast.LENGTH_SHORT).show();
+                    intent = new Intent(ReviewActivity.this, Ar.class);
+                    startActivity(intent);
+                    break;
+                case R.id.nutrition_item:
+                    Toast.makeText(ReviewActivity.this, "Nutrition Info", Toast.LENGTH_SHORT).show();
+                    intent = new Intent(ReviewActivity.this, Infor_window.class);
+                    startActivity(intent);
+                    finish();
+                    break;
             }
+            return true;
         });
 
     }
